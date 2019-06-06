644862cdb802aadea65a170c8b0fe71108cd98b9
Michael Liu <michael.liu0@gmail.com>
2019-06-05 17:11:46 -0700

    Amplify config update

--- src/Amplify-Config.js
+++ src/Amplify-Config.js
@@ -1,10 +1,22 @@
-import Amplify from "aws-amplify";
+import Auth from "@aws-amplify/auth";
+import API from "@aws-amplify/api";
 
-Amplify.configure({
+Auth.configure({
     Auth:{
         mandatorySignIn: false,
         region: process.env.COGNITO_REGION,
         userPoolId: process.env.COGNITO_USER_POOL_ID,
         userPoolWebClientId: process.env.APP_CLIENT_ID
+    }
+});
+
+API.configure({
+    API:{
+        endpoints:[
+            {
+                name: "hex",
+                endpoint: process.env.API_URL
+            }
+        ]
     }
 });
