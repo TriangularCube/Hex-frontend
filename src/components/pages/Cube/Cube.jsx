5f607e88bf8a5acf22fe3365921637774ad361a5
Michael Liu <michael.liu0@gmail.com>
2019-08-30 21:26:44 -0700

    Minor work on Cube and CubeEdit

--- src/components/pages/Cube/Cube.jsx
+++ src/components/pages/Cube/Cube.jsx
@@ -1,5 +1,11 @@
 import React, {useState} from "react";
 
+import {useAsync} from "react-async-hook";
+
+import amp from "../../../util/amplify/amp";
+
+import FittedImage from "react-fitted-image";
+
 // Material UI Util
 import { makeStyles } from "@material-ui/styles";
 
@@ -10,49 +16,112 @@ import {
     Paper,
     Tabs,
     Tab,
-    Divider
+    Divider,
+    Button,
+    Typography
 } from "@material-ui/core";
 
 // Hex Components
 import PageTitle from "../../common/PageTitle";
+import PageLoading from "../../common/PageLoading";
+
 
 const useStyles = makeStyles((theme) => ({
+    topRowContainer: {
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    spacer: {
+        flex: 1
+    },
+    editButton: {
+        height: theme.fontSize,
+        alignSelf: 'center'
+    },
     cubeListTab: {
         display: 'flex',
         flexDirection: 'row'
     },
-    paper: {
+    cubeDescriptionBackground: {
         padding: theme.spacing(2),
-        textAlign: 'center',
+        // textAlign: 'center',
         color: theme.palette.text.secondary,
+    },
+    coverImage: {
+        width: '100%',
+        height: '100%'
     },
     divider: {
         marginBottom: theme.spacing( 2 )
     }
 }));
 
-const Cube = () => {
+const Cube = ( props ) => {
+
+    const handle = props.match.params.handle;
+
+    // Get the cube
+    const asyncGetCube = useAsync( async () => {
+        return await amp.Get( `/cube/${handle}` );
+    }, [] );
 
     const classes = useStyles();
 
     // Tab Support
     const [tabValue, setTabValue] = useState( 0 );
 
+    // Back out while loading
+    if( asyncGetCube.loading ){
+        return <PageLoading/>
+    }
+
+    //region Errors
+    if( asyncGetCube.error ){
+        // DEBUG
+        console.log( 'Error loading cube, message ' + asyncGetCube.error );
+
+        // TODO
+        return null;
+    }
+
+    if( !asyncGetCube.result.success ){
+        // DEBUG
+        console.log( 'Error fetching the cube, message from server ' + asyncGetCube.result.error );
+
+        // TODO
+        return null;
+    }
+    //endregion
+
+    const cube = asyncGetCube.result.cube;
+    // TODO
+
     const Description = () => {
         return(
             <Grid container spacing={1}>
-                <Grid item xs={12} sm={4} lg={3}>
-                    <Paper className={classes.paper}>
-                        <p>
-                            Cube Page
-                        </p>
+                <Grid item xs={12} sm={4} md={3}>
+                    <Paper className={classes.cubeDescriptionBackground}>
+
+                        <img
+                            src='http://placekitten.com/500/500'
+                            alt='Cube Image'
+                            className={classes.coverImage}
+                        />
+
+                        <Typography variant='h5'>
+                            {cube.name}
+                        </Typography>
+
                     </Paper>
                 </Grid>
-                <Grid item xs={12} sm={8} lg={9}>
-                    <Paper className={classes.paper}>
-                        <p>
-                            Cube Description
-                        </p>
+                <Grid item xs={12} sm={8} md={9}>
+                    <Paper className={classes.cubeDescriptionBackground}>
+                        <div>
+                            <p>
+                                Cube Description
+                            </p>
+                        </div>
                     </Paper>
                 </Grid>
             </Grid>
@@ -67,11 +136,29 @@ const Cube = () => {
         )
     };
 
+    const handleEditButton = () => {
+        props.history.push( `${handle}/edit` );
+    };
+
     return(
         <Container maxWidth='lg'>
-            <PageTitle>
-                Cube Name
-            </PageTitle>
+            {/* The Top Row */}
+            <div className={classes.topRowContainer}>
+                {/* Page Title */}
+                <PageTitle>
+                    Cube Name
+                </PageTitle>
+
+                {/* Spacer to fill row */}
+                <div className={classes.spacer}/>
+
+                {/* New Cube Button */}
+                <div className={classes.editButton}>
+                    <Button variant='outlined' onClick={ handleEditButton }>
+                        Edit Cube
+                    </Button>
+                </div>
+            </div>
 
             <Tabs
                 value={tabValue}
--- src/components/pages/Cube/CubeEdit.jsx
+++ src/components/pages/Cube/CubeEdit.jsx
@@ -1,9 +1,69 @@
 import React from "react";
 
-const CubeEdit = () => {
+import { useAsync } from "react-async-hook";
+
+// Redux
+import { useSelector } from "react-redux";
+
+// Amp
+import amp from "../../../util/amplify/amp";
+
+// Hex components
+import PageLoading from "../../common/PageLoading";
+
+const CubeEdit = ( props ) => {
+
+    const handle = props.match.params.handle;
+
+    const user = useSelector( state => state.user );
+
+    const asyncGetCube = useAsync( async () => {
+        return await amp.GetWithAuth( `/cube/${handle}` );
+    }, [] );
+
+    if( asyncGetCube.loading ){
+        return <PageLoading />
+    }
+
+    //region Errors
+    if( asyncGetCube.error ){
+        // DEBUG
+        console.error( 'Error getting cube, message: ', asyncGetCube.error );
+
+        // TODO
+        return null;
+    }
+
+    if( !asyncGetCube.result.success ){
+        // DEBUG
+        console.error( 'Error fetching cube from server, server message: ', asyncGetCube.result.error );
+
+        // TODO
+        return null;
+    }
+    //endregion
+
+    const cube = asyncGetCube.result.cube;
+
+    // DEBUG
+    console.log( 'Cube: ', cube );
+    console.log( 'User: ', user );
+
+    // FIXME This is maybe too naive an implementation?
+    if( cube.owner.displayName !== user.displayName ){
+        console.error( 'Owner of Cube and current user do not match' );
+
+        // TODO
+        return (
+            <p>
+                User and Cube Owner mismatch
+            </p>
+        )
+    }
+
     return(
         <p>
-            Cube Edit Page
+            Edit Cube!
         </p>
     )
 };
